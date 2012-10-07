ac7eef0f07acdd82795f3c89faf0cc3ee386fe4c
Ryuichi <ryu@Ryuichi-no-Mac-mini.local>
2012-10-07 20:22:46 +0900

    first commit

---
+++ event_cal/NSManagedObject_Event.h
@@ -0,0 +1,13 @@
+//
+//  NSManagedObject_Event.h
+//  event_cal
+//
+//  Created by Ryuichi on 12/10/07.
+//  Copyright (c) 2012年 Ryuichi. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObject ()
+
+@end
