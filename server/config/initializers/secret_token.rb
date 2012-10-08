2753a4ca8b72335a4bfc1fb0ccc4496e093776b1
Ryuichi Kato <ryu00026@gmail.com>
2012-10-08 22:22:22 +0900

    サーバサイトプログラム実装開始

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '8aba39b062f0c7cb8f7bf585d1e001cd5b46bbd6260c160cbaa8a8a2872d9960798b68224533e8ab2bca0d62cc910a2dd63d09f504b2ee39dde67a65b64ad80f'
